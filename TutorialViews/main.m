b9619168a9b3760cfe6f7abfe9eef14c0c076793
Daigo <marcelo.daigo@gmail.com>
2012-10-26 01:27:14 -0200

    Initial Commit

---
+++ TutorialViews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TutorialViews
+//
+//  Created by Daigo Matsuoka on 26/10/12.
+//  Copyright (c) 2012 iMasters Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
